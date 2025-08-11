cc880d503f8f9890bb336d83fcebcf99874ab481
culi <culitif@tuta.io>
2025-08-11 14:28:42 -0700

    feat: preload assets for imgly

--- app/pages/makecard/utils/removeBackgroundFromUrl.ts
+++ app/pages/makecard/utils/removeBackgroundFromUrl.ts
@@ -1,14 +1,25 @@
-import { removeBackground } from '@imgly/background-removal';
+import { preload, removeBackground, type Config } from '@imgly/background-removal';
+
+const config: Config = {
+  progress: (key, curr, total) => {
+    console.log(`Preloading ${key}: ${curr} of ${total}`);
+  }
+};
+preload(config).then(() => {
+  console.log("Asset preloading succeeded");
+});
 
 const cache: Record<string, string> = {};
-const removeBackgroundFromUrl = async (url: string) => {
-  if (!cache[url]) {
-    cache[url] = 'loading';
+const removeBackgroundFromUrl = async (url: string | Blob) => {
+  const cacheKey = typeof url === 'string' ? url : URL.createObjectURL(url);
+
+  if (!cache[cacheKey]) {
+    cache[cacheKey] = 'loading';
     const src = URL.createObjectURL(await removeBackground(url));
-    cache[url] = src;
+    cache[cacheKey] = src;
   }
 
-  return cache[url];
+  return cache[cacheKey];
 };
 
 export default removeBackgroundFromUrl;
