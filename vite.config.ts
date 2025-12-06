6d26902108113f58a9a98e066199e8c934d6884a
culi <culitif@tuta.io>
2025-12-05 20:31:35 -0800

    give up on netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,19 @@
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import wyw from "@wyw-in-js/vite";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
-export default defineConfig(async () => {
-  const tsconfigPaths = (await import("vite-tsconfig-paths")).default;
-  const netlifyReactRouterPlugin = (await import("@netlify/vite-plugin-react-router")).default;
-  const netlifyVitePlugin = (await import('@netlify/vite-plugin')).default;
-
-  return {
-    plugins: [
-      wyw({
-        include: ["**/*.{ts,tsx}"],
-        babelOptions: {
-          presets: ["@babel/preset-typescript", "@babel/preset-react"],
-        },
-      }),
-      reactRouter(),
-      tsconfigPaths(),
-      netlifyReactRouterPlugin(),
-      netlifyVitePlugin(),
-    ],
-    build: { target: "es2022", },
-  }
+export default defineConfig({
+  plugins: [
+    wyw({
+      include: ["**/*.{ts,tsx}"],
+      babelOptions: {
+        presets: ["@babel/preset-typescript", "@babel/preset-react"],
+      },
+    }),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+  build: { target: "es2022", },
 });
